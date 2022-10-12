71bd0983599286dd38b6c6dac2d7d26fd1f76310
Golo_er38@student.itstep.org <Golo_er38@student.itstep.org>
2022-10-12 20:37:39 +0300

    0

--- C_Sharp_HW_04_Library_Anton_Golovash_01.10.2022/Fraction.cs
+++ C_Sharp_HW_04_Library_Anton_Golovash_01.10.2022/Fraction.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,35 +16,52 @@ namespace C_Sharp_HW_05_Library_Anton_Golovash_08._10._2022
             chislitel = a;
             znamenatel = b;
         }
-
         public static Fraction operator + (Fraction fr1, Fraction fr2)
         {
             Fraction fraction = new Fraction
                 (fr1.chislitel + fr2.chislitel, fr1.znamenatel + fr2.znamenatel);
             return fraction;
         }
-
         public static Fraction operator - (Fraction fr1, Fraction fr2)
         {
             Fraction fraction = new Fraction
                 (fr1.chislitel - fr2.chislitel, fr1.znamenatel - fr2.znamenatel);
             return fraction;
         }
-
         public static Fraction operator * (Fraction fr1, Fraction fr2)
         {
             Fraction fraction = new Fraction
                 (fr1.chislitel * fr2.chislitel, fr1.znamenatel * fr2.znamenatel);
             return fraction;
         }
-
         public static Fraction operator / (Fraction fr1, Fraction fr2)
         {
             Fraction fraction = new Fraction
                 (fr1.chislitel / fr2.chislitel, fr1.znamenatel / fr2.znamenatel);
             return fraction;
         }
-
+        public static bool operator ==(Fraction fr1, Fraction fr2)
+        {
+            if (fr1.chislitel == fr2.chislitel && fr1.znamenatel == fr2.znamenatel)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static bool operator !=(Fraction fr1, Fraction fr2)
+        {
+            if (fr1.chislitel != fr2.chislitel || fr1.znamenatel != fr2.znamenatel)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
 
     }
 }
